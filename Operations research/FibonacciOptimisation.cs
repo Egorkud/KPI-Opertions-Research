718eeb45279721e590644557dbdaf885e70d16b1
Egorkud <91795863+Egorkud@users.noreply.github.com>
2024-03-28 00:12:51 +0200

    Add files via upload

---
+++ Operations research/FibonacciOptimisation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations_research
+{
+    // Лабораторна робота 1 Кудрявцев Єгор Олегович
+    public class FibonacciOptimisation
+    {
+        // Метод для знаходження мінімуму функції за допомогою методу Фібоначчі
+        public double Optimisation(double a, double b, int n)
+        {
+            // Задання точності
+            double EPS = 0.00001;
+
+            // Ініціалізація змінних
+            double x1, x2;
+            double q = b - a;
+            int i = 1;
+            string separator = "|........|........................|........|"; // 8 24 8
+
+            // Пошук необхідного значення n для методу Фібоначчі
+            while (Fibonacci(n + 2) <= q / EPS)
+            {
+                n++;
+            }
+
+            // Обчислення початкових точок x1 та x2
+            x1 = a + (double)Fibonacci(n) / Fibonacci(n + 2) * (b - a);
+            x2 = a + (double)Fibonacci(n + 1) / Fibonacci(n + 2) * (b - a);
+
+            // Гарний друк таблиці
+            Console.WriteLine($"Iteration           [a, b]           f(x)\n{separator}");
+
+            // Ітеративний процес методу Фібоначчі
+            while (i <= n - 1)
+            {
+                // Зміна границь інтервалу та пресування точок
+                if (Function(x1) < Function(x2))
+                {
+                    b = x2;
+                    x2 = x1;
+                    x1 = a + (double)Fibonacci(n - i) / Fibonacci(n - i + 2) * (b - a);
+                }
+                else
+                {
+                    a = x1;
+                    x1 = x2;
+                    x2 = a + (double)Fibonacci(n - i + 1) / Fibonacci(n - i + 2) * (b - a);
+                }
+
+                // Гарний друк даних
+                Console.WriteLine($"|{i,7} |{a,10:F6},   {b,-10:F6}| {Function((a + b) / 2),-7:F5}|\n{separator}");
+                i++;
+            }
+
+            // Повернення середини останнього інтервалу як результату оптимізації
+            return (a + b) / 2;
+        }
+
+        // Обчислення значення функції завдання f(x) = 3*x^4 + (x - 1)^2
+        public double Function(double x)
+        {
+            return 3 * Math.Pow(x, 4) + Math.Pow(x - 1, 2);
+        }
+
+        // Рекурсивна функція для обчислення числа Фібоначчі
+        public int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
---
+++ Operations research/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Operations_research
+{
+    public abstract class Program
+    {
+        // Усі лабораторні викликаю з відповідних класів
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Введення значень
+            double a = 0; // Ліва межа інтервалу
+            double b = 4; // Права межа інтервалу
+            int n = 1;    // Початкове значення n
+
+            // Створення змінних обчислень Лаб 1
+            FibonacciOptimisation Lab1 = new FibonacciOptimisation();
+            double resultFibo = Lab1.Optimisation(a, b, n);
+            double resultFunc = Lab1.Function(resultFibo);
+
+            // Друк результату та обчислень
+            Console.WriteLine($"\nМінімум функції методом Фібоначчі:\nx = {resultFibo}\n");
+            Console.WriteLine($"Мінімум функції:\nf(x) = {resultFunc}");
+        }
+    }
+}
